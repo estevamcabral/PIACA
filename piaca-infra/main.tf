fcdf884484457ecc7311913d02fb977e1f5dab2c
estevamcabral <estevamcabral1308@gmail.com>
2026-03-15 01:42:43 -0300

    terraform e docker compose

--- piaca-infra/main.tf
+++ piaca-infra/main.tf
@@ -2,47 +2,59 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_key_pair" "key" {
+  key_name   = "piaca-key"
+  public_key = file("C:/Users/estev/.ssh/id_ed25519.pub")
+}
+
 resource "aws_security_group" "sg" {
   name = "piaca-sg"
 
   ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["189.6.248.124/32"]
+  }
+
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "ec2" {
-  ami = "ami-0c02fb55956c7d316"
-  instance_type = "t3.micro"
-
+  ami                         = "ami-0c02fb55956c7d316"
+  instance_type               = "t3.micro"
+  key_name                    = aws_key_pair.key.key_name
   associate_public_ip_address = true
-  vpc_security_group_ids = [aws_security_group.sg.id]
+  vpc_security_group_ids      = [aws_security_group.sg.id]
 
   user_data = <<EOF
 #!/bin/bash
+yum update -y
+yum install -y docker git
 
-apt update -y
-apt install -y docker.io docker-compose git
-
+service docker start
 systemctl enable docker
-systemctl start docker
+usermod -aG docker ec2-user
 
-cd /home/ubuntu
+curl -L https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
+cd /home/ec2-user
 git clone https://github.com/estevamcabral/PIACA.git app
-
 cd app
-
-docker compose up -d
+docker-compose up -d
 EOF
 
   tags = {
